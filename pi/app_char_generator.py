b143c61512a86ceab14ea1800f74ed1282aa779b
PietVlem <pieter.vleminckx@student.arteveldehs.be>
2018-10-03 23:48:17 +0200

    unfinished

--- pi/app_char_generator.py
+++ pi/app_char_generator.py
@@ -1,33 +1,44 @@
 from time import sleep
+from sense_hat import SenseHat
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
 #firebase init
-cred = credentials.Certificate("services/charactergenerator-a88a7-firebase-adminsdk-d3wy6-81d801feb9.json")
-firebase_admin.initialize_app(cred)
+cred = credentials.Certificate("charactergenerator-a88a7-firebase-adminsdk-d3wy6-81d801feb9.json")
+firebase = firebase_admin.initialize_app(cred,{'databaseURL':'https://charactergenerator-a88a7.firebaseio.com'})
+
+sense = SenseHat()
+sense.clear()
+
 
 while True:
     #firebase vars
-    color = db.reference().child('color')
-    r = 0
-    g = 0
-    b = 0
+    color = db.reference('/color').get()
+    r = color['r']
+    g = color['g']
+    b = color['b']
+    print(r)
 
-    matrix = db.reference().child('matrix')
-
-    print(color)
-    print(matrix)
+    matrix = db.reference('/matrix').get()
+    #print(matrix)
 
     #color lights
-    col = 0
+    col = 0;
+    row = 0;
     for row in matrix:
-        for val in matrix
-            if val = "color":
-                sense.set_pixel(row, col, r, g, b)
+        for val in row:
+            if val == "color":
+                sense.set_pixel(row, col, int(r), int(g), int(b))
+            print(col)
             col = col + 1
-
-    sleep(1)
+            if col > 7:
+                col = 0
+        row = row + 1
+        if row > 7:
+            row = 0
+            
+    sleep(10)
 
 
 
